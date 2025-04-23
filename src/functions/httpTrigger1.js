388fc85cb6851cf36731ef4bb1092c3a002c7cb1
thamim1998 <thamim1997@gmail.com>
2025-04-23 23:46:03 +0200

    function app connected to Azure SQL

--- src/functions/httpTrigger1.js
+++ src/functions/httpTrigger1.js
@@ -1,32 +1,72 @@
-const { app } = require('@azure/functions');
+const { app } = require("@azure/functions");
 
-app.http('httpTrigger1', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
-    handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
+const sql = require("mssql");
 
-        const name = request.query.get('name') || await request.text() || 'world';
+const sqlConfig = {
+  user:"",
+  options: {
+    encrypt: true,
+  },
+};
 
-        return { body: `Hello, ${name}!` };
-    }
+app.http("httpTrigger1", {
+  methods: ["GET", "POST"],
+  authLevel: "anonymous",
+  handler: async (request, context) => {
+    context.log(`Http function processed request for url "${request.url}"`);
+
+    const name = request.query.get("name") || (await request.text()) || "world";
+
+    return { body: `Hello, ${name}!` };
+  },
 });
 
-app.http('getExample', {
-    methods: ['GET'],
-    authLevel: 'anonymous',
-    handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
+app.http("getExample", {
+  methods: ["GET"],
+  authLevel: "anonymous",
+  handler: async (request, context) => {
+    context.log(`Http function processed request for url "${request.url}"`);
 
-        const name = request.query.get('name') || 'Thamim';
-        const age = request.query.get('age'); 
+    const name = request.query.get("name") || "Thamim";
+    const age = request.query.get("age");
 
-        let responseMessage = `Hello ${name}!`;
-        if(age){
-            responseMessage += `You are ${age} years old.`
-        }
-
-
-        return { body: responseMessage };
+    let responseMessage = `Hello ${name}!`;
+    if (age) {
+      responseMessage += `You are ${age} years old.`;
     }
+
+    return { body: responseMessage };
+  },
+});
+
+app.http("getCourse", {
+  methods: ["GET"],
+  authLevel: "anonymous",
+  handler: async (request, context) => {
+    context.log("Fetching course from Azure SQL DB");
+
+    context.log(`USERNAME: ${process.env["SQL_USERNAME"]}`)
+
+    try {
+      await sql.connect(sqlConfig);
+
+      const result = await sql.query`SELECT * FROM Course`;
+
+      return {
+        status: 200,
+        jsonBody: result.recordset,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+    } catch (err) {
+      context.log("SQL error:", err);
+      return {
+        status: 500,
+        body: "Error fetching courses from database",
+      };
+    } finally {
+      sql.close();
+    }
+  },
 });
